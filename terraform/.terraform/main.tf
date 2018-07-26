fc7da1cf804fbfff0d186dbcde2efa3d4f23b756
Navraj Singh Chhina <navuchhina@live.com>
2018-07-25 23:24:04 -0400

    Terraform main added broilerplate added

---
+++ terraform/.terraform/main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  access_key = ""
+  secret_key = ""
+  region     = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-b374d5a5"
+  instance_type = "t2.micro"
+}
+resource "aws_eip" "ip" {
+  instance = "${aws_instance.example.id}"
+}
